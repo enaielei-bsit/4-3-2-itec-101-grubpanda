baa2fd794c8daf91b4cc304c5dd1f266519420d9
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-04-23 23:06:55 +0800

    Add redirection for Products and Menus

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,8 @@
 class MenusController < ApplicationController
+    before_action() do |controller|
+        action = controller.action_name
+        if ["new", "create", "destroy"].include?(action) && !@signed_in
+            redirect_to(new_session_url())
+        end
+    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+    before_action() do |controller|
+        action = controller.action_name
+        if ["new", "create", "destroy"].include?(action) && !@signed_in
+            redirect_to(new_session_url())
+        end
+    end
 end
